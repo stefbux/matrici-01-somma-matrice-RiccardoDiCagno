e86eab739ab4d69bef50d7878f64ccea2879d039
RiccardoDiCagno <riccardo.dicagno@marconibari.edu.it>
2025-03-13 09:37:09 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 using namespace std;
+int main() 
+{
+int matrice [3][3],somma=0;
+cout<<"inserisci i  valori della matrice 3*3:"<<endl;
+for(int i=0; i<3; i++) {
+cout<<"inserisci elemento in posizione ["<<i<<"]["<<j<<"]:";
+cin>> matrice[i][j];}
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+
 }
+
+
+
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
